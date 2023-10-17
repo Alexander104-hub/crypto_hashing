827c9326623e31b163e15843c94046617ad4f3f4
Alonso-Developer <nveretilo@bk.ru>
2023-10-18 00:06:38 +0300

    Key, tag and cipher.nonce now represented as hex.

--- app/utils/encryption.py
+++ app/utils/encryption.py
@@ -1,14 +1,14 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 import base64
-import ast
+import binascii
 
-def encrypt(text: str) -> str:
+def encrypt(text):
     text = text.encode('utf-8')
     random_key = get_random_bytes(16)
     cipher = AES.new(random_key, AES.MODE_EAX)
     ciphertext, tag = cipher.encrypt_and_digest(text)
-    return base64.b64encode(ciphertext).decode('utf-8'), bytes_to_str(random_key), bytes_to_str(tag), bytes_to_str(cipher.nonce)
+    return base64.b64encode(ciphertext).decode('utf-8'), bytes_to_str(binascii.hexlify(random_key)), bytes_to_str(binascii.hexlify(tag)), bytes_to_str(binascii.hexlify(cipher.nonce))
 
 def bytes_to_str(b: bytes):
     return str(b)[2:-1]
