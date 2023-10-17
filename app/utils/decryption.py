4d83dca6826855a7816674fc983b9f57677341ef
Alonso-Developer <nveretilo@bk.ru>
2023-10-18 00:05:39 +0300

    bytes_from_str function now requires hex string

--- app/utils/decryption.py
+++ app/utils/decryption.py
@@ -1,10 +1,9 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 import base64
-import ast
-import codecs
+import binascii
 
-def decrypt(ciphertext: str, key: str, tag: str, nonce: str):
+def decrypt(ciphertext, key, tag, nonce):
     key = bytes_from_str(key)
     tag = bytes_from_str(tag)
     nonce = bytes_from_str(nonce)
@@ -14,4 +13,5 @@ def decrypt(ciphertext: str, key: str, tag: str, nonce: str):
     return decrypted_bytes.decode('utf-8')
 
 def bytes_from_str(raw_str: str):
-    return raw_str.replace('\\\\', '\\').encode().decode('unicode_escape').encode('latin1')
+    # return raw_str.replace('\\\\', '\\').encode().decode('unicode_escape').encode('latin1')
+    return binascii.unhexlify(raw_str)
