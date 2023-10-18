82da3781044f2b7b12cea0e39223a1fb28f337dd
Alexander104-hub <pudlovskiy2018@mail.ru>
2023-10-18 16:28:34 +0300

    Reverted changes in the file to its previous state.

--- app/utils/hash/file_hashing.py
+++ app/utils/hash/file_hashing.py
@@ -17,8 +17,8 @@ class Hash:
         self.__hashes = hashes
         with open("./hashes.json", "w") as file:
             json.dump(self.__hashes, file, indent=4)
-                        
-                            
+
+
     def __dir_to_list(self, dirname, path=os.path.pathsep):
         data = {}
         for file_name in os.listdir(dirname):
@@ -29,15 +29,7 @@ class Hash:
                 data[file_name] = self.__get_hash(fullpath)
         return data
 
-    def get_hash(self, file_path):
-        sha256_hash = hashlib.sha256()
-        with open(file_path,"rb") as f:
-            for byte_block in iter(lambda: f.read(4096),b""):
-                sha256_hash.update(byte_block)
-        return sha256_hash.hexdigest()
-
-    
     def __get_hash(self, file: str):
-        digest = self.get_hash(file)
-        return digest
-
+        with open(file, "rb") as out_file:
+            digest = hashlib.file_digest(out_file, "sha256")
+        return digest.hexdigest() 
